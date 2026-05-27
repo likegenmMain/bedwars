8c309e6d16f4d542db2e36d4edf10ec35a5e1a62
likegenmMain <likegenmscripts@gmail.com>
2026-05-27 21:28:15 +0300

    Update loader.lua

--- Bedwars/loader.lua
+++ Bedwars/loader.lua
@@ -1,11 +1,13 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/likegenmMain/bedwars/refs/heads/main/Bedwars/Script.lua", true))()
 
-local queue_on_teleport = queue_on_teleport or syn.queue_on_teleport or fluxus.queue_on_teleport
+local queue_on_teleport = queue_on_teleport or (syn and syn.queue_on_teleport) or (fluxus and fluxus.queue_on_teleport) or queueonteleport
 
 if queue_on_teleport then
-    queue_on_teleport([[
+    local teleportCode = [[
+        task.wait(1)
         if game.GameId == 2619619496 then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/likegenmMain/bedwars/refs/heads/main/Bedwars/Script.lua", true))()
         end
-    ]])
+    ]]
+    queue_on_teleport(teleportCode)
 end
